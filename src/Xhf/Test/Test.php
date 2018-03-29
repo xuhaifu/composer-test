e4fee0974661a1ea1cc0c10723ff04a3a06bf0d8
xuhaifu <xuhaifu@banggood.com>
2018-03-29 11:20:38 +0800

    init

---
+++ src/Xhf/Test/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Test.php
+ * Author: XHF
+ * Date: 2018/3/29
+ * Time: 11:13
+ */
+namespace Xhf\Test;
+
+class Test
+{
+    public static function info()
+    {
+        echo "This is test!<br />";
+    }
+}
