de41f9ad8d1aef77b6d0a83440269cec441ee4f5
xuhaifu <xuhaifu@banggood.com>
2018-05-11 15:27:01 +0800

    添加

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by index.php
+ * Author: XHF
+ * Date: 2018/5/11
+ * Time: 14:40
+ */
+
+require 'vendor/autoload.php';
+
---
+++ lib/ClassTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by ClassTest.php
+ * Author: XHF
+ * Date: 2018/3/29
+ * Time: 16:06
+ */
+
+class ClassTest
+{
+    public function index()
+    {
+        echo "hello world!";
+    }
+}
